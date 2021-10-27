49f2b3cc1cb81e935b92e20ff7be347b78a30888
刘章 <shiweike63218@163.com>
2021-10-27 16:50:56 +0800

    新增了一个类

---
+++ src/main/java/com/atguigu/pojo/ABC.java
@@ -0,0 +1,12 @@
+package com.atguigu.pojo;
+
+/**
+ * @author shkstart
+ * @create 2021-10-27 16:50
+ */
+public class ABC {
+
+    public static void main(String[] args) {
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAA");
+    }
+}
